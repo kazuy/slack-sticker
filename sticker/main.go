95a0a8b7da260022f5cd4a71cd2d1e35df0fd177
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2020-03-07 22:36:46 +0900

    Lambda environment from SSM

--- sticker/main.go
+++ sticker/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
-	"log"
 	"encoding/json"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -21,12 +23,26 @@ type Response events.APIGatewayProxyResponse
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
     sc := QueryParameterToSlashCommand(request.Body)
 
-    msg := Message(sc)
+    if !sc.ValidateToken(os.Getenv("SLASH_TOKEN")) {
+        log.Fatal("Token Error")
+    }
+
+    sticker := PickUpSticker(sc.Text)
+    log.Printf("sticker: %s \n", sticker)
+
+    image := GetStickerImage(sticker)
+    log.Printf("image: %s \n", image)
+
+    user := GetUserProfile(sc.UserID)
+    log.Printf("user: %s, %s, %s \n", user.DisplayName, user.RealName, user.Image72)
+
+    PostMessage(sc.ChannelName, user.RealName, user.Image72, image)
+    log.Printf("Post Message \n")
 
 	resp := Response{
-		StatusCode:      200,
+		StatusCode:      204,
 		IsBase64Encoded: false,
-		Body:            msg,
+		Body:            "",
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
 			"X-MyCompany-Func-Reply": "sticker-handler",
@@ -57,15 +73,45 @@ func QueryParameterToSlashCommand(q string) *slack.SlashCommand {
     return s
 }
 
-func Message(sc *slack.SlashCommand) string {
-    params := &slack.Msg {
-        Text: sc.Text,
-    }
+func PickUpSticker(str string) string {
+    return strings.Replace(str, ":", "", -1)
+}
 
-    b, err := json.Marshal(params)
+func GetStickerImage(key string) string {
+    api := slack.New(os.Getenv("LEGACY_TOKEN"))
+    emojis, err := api.GetEmoji()
     if err != nil {
-        log.Fatal("Convert failed: ", err)
+        log.Fatal("error: %s", err)
+    }
+    return emojis[key]
+}
+
+func GetUserProfile(key string) *slack.UserProfile {
+    api := slack.New(os.Getenv("LEGACY_TOKEN"))
+    profile, err := api.GetUserProfile(key, false)
+    if err != nil {
+        log.Fatal("Get Error: ", err)
+    }
+    return profile
+}
+
+func PostMessage(channel string, userName string, userIcon string, image string) {
+    log.Printf("channel: %s, image: %s \n", channel, image)
+
+    message := slack.WebhookMessage {
+        Text: " ",
+        Channel: "#" + channel,
+        Username: userName,
+        IconURL: userIcon,
+        Attachments: []slack.Attachment {
+            slack.Attachment {
+                ImageURL: image,
+            },
+        },
     }
 
-    return string(b)
+    err := slack.PostWebhook(os.Getenv("WEBHOOK_URL"), &message)
+    if err != nil {
+        log.Fatal("Get Error: ", err)
+    }
 }
